96dc1333637948d2bf78da631b1ce638e7309f77
Caelan <s10196427@connect.np.edu.sg>
2021-11-18 18:56:58 +0800

    Add interactive console menu
    Finally, a clean way to write menus.

---
+++ SimpCity/ConsoleMenu.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpCity {
+    class ConsoleMenuOption {
+        public string Description { get; }
+        public Action Callback { get; }
+
+        public ConsoleMenuOption(string description, Action callback) {
+            this.Description = description;
+            this.Callback = callback;
+        }
+    }
+
+    /// <summary>
+    /// Creates an interactive console menu
+    /// </summary>
+    class ConsoleMenu {
+        public List<ConsoleMenuOption> Options;
+        public ConsoleMenu(List<ConsoleMenuOption> options) {
+            this.Options = options;
+        }
+
+        private string GetMenuText() {
+            string buf = "";
+            for (int i = 0; i < this.Options.Count; i++) {
+                int opt = i + 1;
+                buf += opt.ToString() + ") " + this.Options[i].Description + "\n";
+            }
+            buf += "\n0) Exit\n";
+            return buf;
+        }
+
+        /// <summary>
+        /// Displays the menu
+        /// </summary>
+        public void Display() {
+            Console.WriteLine(GetMenuText());
+        }
+
+        /// <summary>
+        /// Asks for user input to execute the callback. Returns <i>true</i> if the user intends to exit.
+        /// </summary>
+        public bool AskInput() {
+            bool exit = false;
+
+            Console.Write("Enter your option: ");
+            if (!int.TryParse(Console.ReadLine(), out var option)) {
+                Console.WriteLine("Invalid option");
+                goto end;
+            }
+
+            if (option == 0) {
+                exit = true;
+                goto end;
+            }
+
+            if (option > 0 && option <= this.Options.Count) {
+                ConsoleMenuOption consoleOpt = this.Options[option - 1];
+                // Print the option the user picked
+                Console.WriteLine("Option " + option + ". " + consoleOpt.Description);
+                Console.WriteLine(new string('-', 26));
+                // Run the action
+                consoleOpt.Callback();
+            }
+
+end:
+            return exit;
+        }
+    }
+}
--- SimpCity/Program.cs
+++ SimpCity/Program.cs
@@ -7,7 +7,17 @@ using System.Threading.Tasks;
 namespace SimpCity {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Welcome to SimpCity!");
+            bool exit = false;
+            while (exit == false) {
+                Console.WriteLine("Welcome, mayor of Simp City!\n" + new string('=', 26));
+                ConsoleMenu menu = new ConsoleMenu(new List<ConsoleMenuOption> {
+                    new ConsoleMenuOption("Start new game", () => Console.WriteLine("TODO new game")),
+                    new ConsoleMenuOption("Load saved game", () => Console.WriteLine("TODO load game"))
+                });
+
+                menu.Display();
+                exit = menu.AskInput();
+            }
         }
     }
 }
