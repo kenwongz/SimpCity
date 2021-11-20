6131f8a97dc174922edab4dd411bb64c1f6f43f5
Caelan <s10196427@connect.np.edu.sg>
2021-11-20 16:46:49 +0800

    Add tests for ConsoleMenu

---
+++ SimpCity/ConsoleMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpCity {
+    public class ConsoleMenuOption {
+        public string Description { get; }
+        public Action Callback { get; }
+
+        public ConsoleMenuOption(string description, Action callback) {
+            this.Description = description;
+            this.Callback = callback;
+        }
+    }
+
+    /// <summary>
+    /// Creates an interactive console menu
+    /// </summary>
+    public class ConsoleMenu {
+        public List<ConsoleMenuOption> Options;
+        public ConsoleMenu(List<ConsoleMenuOption> options) {
+            this.Options = options;
+        }
+
+        protected string GetMenuText() {
+            throw new NotImplementedException();
+        }
+
+
+
+        /// <summary>
+        /// Displays the menu
+        /// </summary>
+        public void Display() {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Asks for user input to execute the callback. Returns <i>true</i> if the user intends to exit.
+        /// </summary>
+        public bool AskInput(int? testOption = null) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpCityTests/ConsoleMenuTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using SimpCity;
+
+
+namespace SimpCityTests {
+    [TestClass]
+    public class ConsoleMenuTests {
+        [TestMethod]
+        public void AskInput_ExecutesCallbacks() {
+            bool testSwitch = false;
+
+            ConsoleMenu menu = new ConsoleMenu(new List<ConsoleMenuOption> {
+                new ConsoleMenuOption("This is an option", () => {
+                    // change testSwitch to true
+                    testSwitch = true;
+                }),
+            });
+
+            menu.AskInput(1);
+            // Check the switch is changed
+            Assert.IsTrue(testSwitch);
+
+        }
+
+        [TestMethod]
+        public void AskInput_Exits_WhenRequested() {
+            ConsoleMenu menu = new ConsoleMenu(new List<ConsoleMenuOption> {
+                new ConsoleMenuOption("This is an option", () => {}),
+            });
+
+            bool exit;
+
+            // 1 means no exit
+            exit = menu.AskInput(0);
+            Assert.IsTrue(exit);
+
+            // 0 means exit
+            exit = menu.AskInput(0);
+            Assert.IsTrue(exit);
+
+        }
+    }
+}
