a6fe3bf55e314b57efb1626d59e9a12534b3cf91
Caelan <s10196427@connect.np.edu.sg>
2021-12-07 10:24:29 +0800

    Add test for QA-SN-3

--- SimpCityTests/ConsoleMenuTests.cs
+++ SimpCityTests/ConsoleMenuTests.cs
@@ -66,6 +66,34 @@ namespace SimpCityTests {
             Assert.IsTrue(exit);
         }
 
+        /// <summary>
+        /// QA-SN-3.1:
+        /// This ensures that the function will not break and resumes regular operation when an
+        /// invalid option is supplied.
+        /// </summary>
+        [TestMethod]
+        public void AskInput_RunsNormally_OnInvalidInput() {
+            bool testSwitch = false;
+
+            // Create two options
+            ConsoleMenu menu = new ConsoleMenu()
+                .AddOption("This is an option", (m) => { testSwitch = true; })
+                .AddOption("This is an option 2", (m) => { testSwitch = true; });
+
+            // Input 3, a non-existent option
+            bool exit = menu.AskInput("3");
+
+            // 1. Check the switch is NOT changed
+            Assert.IsFalse(testSwitch, "Test Switch was changed");
+
+            // 2. Will not exit
+            Assert.IsFalse(exit, "Must not exit");
+        }
+
+        /// <summary>
+        /// N/A:
+        /// This ensures that the function throws when trying to edit a non-existent label.
+        /// </summary>
         [TestMethod]
         [ExpectedException(typeof(InvalidOperationException), "Trying to edit a non-existent label did not flag out..")]
         public void EditOption_Throws_WhenInvalidLabel() {
