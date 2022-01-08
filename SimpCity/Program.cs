bf60d0a567aba2892ad19ee22840ebb57c460265
Caelan <s10196427@connect.np.edu.sg>
2022-01-09 00:54:13 +0800

    Display version number in menu
    As part of the bigger goal to provide versioned releases in the future, start
    by displaying the cool version number in the main menu. #33

--- SimpCity/Program.cs
+++ SimpCity/Program.cs
@@ -4,8 +4,17 @@ using System.Reflection;
 namespace SimpCity {
     class Program {
         static void Main(string[] args) {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string informationVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+            
             ConsoleMenu menu = new ConsoleMenu()
-                .BeforeInteraction((m) => Console.WriteLine("Welcome, mayor of Simp City!\n" + new string('=', 28)))
+                .BeforeInteraction((m) => {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"   SimpCity v{informationVersion}   ");
+                    Console.ResetColor();
+                    Console.WriteLine("Welcome, mayor of Simp City!\n" + new string('=', 28));
+                })
                 .AddOption("Start new game", (m) => {
                     Game game = new Game();
                     game.Play();
