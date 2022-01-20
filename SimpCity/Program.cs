06143fee20a9f9d11e0d1dfa1ccd5bf22c0479e7
Caelan <s10196427@connect.np.edu.sg>
2022-01-20 17:52:36 +0800

    feat: Add toggle for test mode
    Disables adjacent rule during test mode. #41

--- SimpCity/Program.cs
+++ SimpCity/Program.cs
@@ -6,23 +6,54 @@ namespace SimpCity {
         static void Main(string[] args) {
             Assembly assembly = Assembly.GetExecutingAssembly();
             string informationVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
-            
+
+            bool isDebugMode = false;
+            Game makeGameFunc() => new Game(new GameOptions {
+                DisableAdjacentRule = isDebugMode,
+            });
+
             ConsoleMenu menu = new ConsoleMenu()
                 .BeforeInteraction((m) => {
                     Console.ForegroundColor = ConsoleColor.Black;
                     Console.BackgroundColor = ConsoleColor.Green;
                     Console.WriteLine($"   SimpCity v{informationVersion}   ");
                     Console.ResetColor();
+
+                    // Notify if the session is in debug
+                    if (isDebugMode) {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"   (Debug mode)   ");
+                        Console.ResetColor();
+                    }
+
                     Console.WriteLine("Welcome, mayor of Simp City!\n" + new string('=', 28));
                 })
                 .AddOption("Start new game", (m) => {
-                    Game game = new Game();
+                    Game game = makeGameFunc();
                     game.Play();
                 })
                 .AddOption("Load saved game", (m) => {
-                    Game game = new Game();
+                    Game game = makeGameFunc();
                     game.Restore();
                     game.Play();
+                })
+                .AddHeading()
+                .AddOption("Toggle debug mode", (m) => {
+                    isDebugMode = !isDebugMode;
+                    Console.Write("Debug mode is now: ");
+                    if (isDebugMode) {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.Write("on");
+                        Console.ResetColor();
+                    } else {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.Write("off");
+                        Console.ResetColor();
+                    }
+                    Console.WriteLine();
                 })
                 .AddHeading()
                 .AddExitOption("Exit");
