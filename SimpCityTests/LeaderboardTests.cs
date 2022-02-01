427986238674afee9597a68e020c64d97bbb76d8
Caelan <s10196427@connect.np.edu.sg>
2022-02-01 14:09:53 +0800

    test: Add new tests to cap records at 10 max

--- SimpCityTests/LeaderboardTests.cs
+++ SimpCityTests/LeaderboardTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SimpCity;
 
@@ -59,7 +60,6 @@ namespace SimpCityTests {
             Assert.IsTrue(scores[0].PlayerName == "Player1" && scores[1].PlayerName == "Player2");
         }
 
-
         /// <summary>
         /// Ensures JSON functionality of the Leaderboard.
         /// </summary>
@@ -97,6 +97,112 @@ namespace SimpCityTests {
 
             // Check data integrity by comparing the exported JSON strings of the two objects
             Assert.IsTrue(glb2.ToJsonString() == encoded);
+        }
+
+        /// <summary>
+        /// Ensures position finding functionality. Leaderboard has space for new records.
+        /// </summary>
+        [TestMethod]
+        public void ScorePointPosition_FindsCorrectPosition_WhenCalledWithSpace() {
+            var glb = new GlobalLeaderboard(null);
+            var lb = glb.GetLeaderboard(4, 4);
+
+            lb.AddScore(new LeaderboardScore {
+                PlayerName = "Player1",
+                Score = 3,
+                Time = new DateTime()
+            });
+
+            lb.AddScore(new LeaderboardScore {
+                PlayerName = "Player2",
+                Score = 2,
+                Time = new DateTime()
+            });
+
+            lb.AddScore(new LeaderboardScore {
+                PlayerName = "Player3",
+                Score = 2,
+                Time = new DateTime()
+            });
+
+            // Score point 4 should be first pos
+            Assert.IsTrue(lb.ScorePointPosition(4) == 1);
+
+            // Score point 2 should be 4th pos
+            Assert.IsTrue(lb.ScorePointPosition(2) == 4);
+        }
+
+        /// <summary>
+        /// Ensures position finding functionality (non-eligible score). Leaderboard has no space
+        /// for new records.
+        /// </summary>
+        [TestMethod]
+        public void ScorePointPosition_ReturnsZero_WhenCalledWithNoSpace() {
+            var glb = new GlobalLeaderboard(null);
+            var lb = glb.GetLeaderboard(4, 4);
+
+            int lowestScore = 30;
+
+            // Create lowest score
+            lb.AddScore(new LeaderboardScore {
+                PlayerName = "Player1",
+                Score = lowestScore,
+                Time = new DateTime()
+            });
+
+            // Create 9 more of such records to fill up space
+            for (int sc = lowestScore; sc < lowestScore + 9; sc++) {
+                lb.AddScore(new LeaderboardScore {
+                    PlayerName = "Player" + sc,
+                    Score = sc,
+                    Time = new DateTime()
+                });
+            }
+
+            // Score point 30 should be non-eligble (out of space)
+            Assert.IsTrue(lb.ScorePointPosition(4) == 0);
+        }
+
+        /// <summary>
+        /// Ensures AddScore removes the last score record, when a higher score is added to a full
+        /// leaderboard, freeing up space.
+        /// </summary>
+        [TestMethod]
+        public void AddScore_RemovesLastScore_WhenCalledWithHigherScoreAndNoSpace() {
+            var glb = new GlobalLeaderboard(null);
+            var lb = glb.GetLeaderboard(4, 4);
+
+            // Create 9 records to fill up space
+            for (int i = 0; i < 9; i++) {
+                lb.AddScore(new LeaderboardScore {
+                    PlayerName = "Player" + i,
+                    Score = 1,
+                    Time = new DateTime()
+                });
+            }
+
+            // Add the 10th player who should be removed later
+            lb.AddScore(new LeaderboardScore {
+                PlayerName = "PlayerShouldBeRemoved",
+                Score = 1,
+                Time = new DateTime()
+            });
+
+            // Sanity check that our PlayerShouldBeRemoved is the last
+            Assert.IsTrue(lb.FlattenScores().Last().PlayerName == "PlayerShouldBeRemoved");
+
+            // Sanity check that our new score should be first pos
+            Assert.IsTrue(lb.ScorePointPosition(2) == 1);
+
+            // Add a higher score
+            lb.AddScore(new LeaderboardScore {
+                PlayerName = "PlayerAtas",
+                Score = 2,
+                Time = new DateTime()
+            });
+
+            // "PlayerShouldBeRemoved" should no longer exist
+            Assert.IsTrue(lb.FlattenScores().Last().PlayerName != "PlayerShouldBeRemoved");
         }
     }
 }
