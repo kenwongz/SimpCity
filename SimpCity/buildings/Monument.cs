5a400093c93b017f8099ce737ef7cdf4c46a0d6b
s10193293@connect.np.edu.sg <s10193293@connect.np.edu.sg>
2022-02-04 16:32:07 +0800

    part 1 of monument scoring done

--- SimpCity/buildings/Monument.cs
+++ SimpCity/buildings/Monument.cs
@@ -7,14 +7,17 @@
         public static string Code { get; } = "MON";
         public Monument(BuildingInfo info) : base(info) { }
         public override int CalcScore(ScoreCalculationArchive archive) {
+            //initialise score to 0
             int score = 0;
             int xPos = Position().X;
             int yPos = Position().Y;
+            //corners
             if(xPos == 0 && yPos == 0 || xPos == 0 && yPos == (Info.Grid.Height - 1) || xPos == (Info.Grid.Width - 1) && yPos == (Info.Grid.Height - 1) ||
                 xPos == (Info.Grid.Width - 1) && yPos == 0)
             {
                 score += 2;
             }
+            //anywhere on grid except corners
             else {
                 score += 1;
             }
