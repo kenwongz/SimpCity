acc5bcae0dec2f5215ee282c12891443dd6f6b56
Caelan <s10196427@connect.np.edu.sg>
2022-02-04 11:12:43 +0800

    test: Add tests for asserting park scoring
    * CalcScore_CalculatesCorrectly_ForPark

--- SimpCityTests/CityGridBuildingTests.cs
+++ SimpCityTests/CityGridBuildingTests.cs
@@ -75,5 +75,45 @@ namespace SimpCityTests {
             // Ensure 4 cumulative points
             Assert.IsTrue(b.CalcScore(a) == 4);
         }
+
+        /// <summary>
+        /// This ensures correct score calculation for Park.
+        /// </summary>
+        [TestMethod]
+        public void CalcScore_CalculatesCorrectly_ForPark() {
+            // Requirement: A Park is a new type of building depending on how many
+            // parks are connected to each other(both horizontally and vertically). The
+            // score for a Park is given by the following table
+            // Size  1 2 3 4 5 6 7 8
+            // Score 1 3 8 16 22 23 24 25
+
+            // 3*3 < 8 (needed to test up till size 8
+            Game game = new Game(new GameOptions {
+                GridWidth = 3,
+                GridHeight = 3
+            });
+
+            // Let's simulate this set up (25 points)
+            // PRK PRK PRK
+            // PRK PRK PRK
+            // PRK PRK nul
+            TestUtils.BatchGameBuild(game, new BuildingTypes?[3,3] {
+                { BuildingTypes.Park, BuildingTypes.Park, BuildingTypes.Park },
+                { BuildingTypes.Park, BuildingTypes.Park, BuildingTypes.Park },
+                { BuildingTypes.Park, BuildingTypes.Park, null },
+            });
+
+            // Some sanity check
+            Assert.IsTrue(game.grid.Get(new CityGridPosition(0, 0)).Info.Code == "PRK"
+                && game.grid.Get(new CityGridPosition(2, 2)) == null);
+
+            // Check that we got 25 points
+            Assert.IsTrue(TestUtils.TotalOf(game.CalculateScores()[BuildingTypes.Park]) == 25);
+
+            // Now let's add the last park, which no longer continues in the chain of 8 parks.
+            // Therefore, the last park adds +1 score, which means 26 in total.
+            game.BuildAt(game.buildingInfo[BuildingTypes.Park], new CityGridPosition(2, 2));
+            Assert.IsTrue(TestUtils.TotalOf(game.CalculateScores()[BuildingTypes.Park]) == 26);
+        }
     }
 }
