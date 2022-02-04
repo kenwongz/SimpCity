93823f5ff86f8872d65056ff078d644904e28862
Caelan <s10196427@connect.np.edu.sg>
2022-02-04 13:07:55 +0800

    test: New test requirement for beach scoring #76
    Updated CalcScore_CalculatesCorrectly_ForBeach to check on last column on
    various widths, rather than the fixed 4x4 on col D.

--- SimpCityTests/CityGridBuildingTests.cs
+++ SimpCityTests/CityGridBuildingTests.cs
@@ -9,31 +9,37 @@ namespace SimpCityTests {
         /// </summary>
         [TestMethod]
         public void CalcScore_CalculatesCorrectly_ForBeach() {
-            Game game = new Game(new GameOptions {
-                // Disable adjacent rule for easier testing
-                DisableAdjacentRule = true
-            });
+            // Test various widths, leaving at least one column in the middle for testing
+            for (int width = 3; width <= 10; width++) {
+                Game game = new Game(new GameOptions {
+                    // Disable adjacent rule for easier testing
+                    DisableAdjacentRule = true,
+                    // Use a custom grid width length
+                    GridWidth = width
+                });
 
-            // Requirement: A Beach (BCH) scores 3 points if it is built in column A or column D,
-            // or 1 point otherwise
+                // Requirement: A Beach (BCH) scores 3 points if it is built in column A or the
+                // last column, or 1 point otherwise
 
-            CityGridBuilding lastBuilding;
-            ScoreCalculationArchive a = new ScoreCalculationArchive();  // stub
+                CityGridBuilding lastBuilding;
+                ScoreCalculationArchive a = new ScoreCalculationArchive();  // stub
 
-            // Build on col A
-            lastBuilding = game.BuildAt(game.buildingInfo[BuildingTypes.Beach], new CityGridPosition(0, 0));
-            // Ensure 3 points
-            Assert.IsTrue(lastBuilding.CalcScore(a) == 3);
+                // Build on col A (first col)
+                lastBuilding = game.BuildAt(game.buildingInfo[BuildingTypes.Beach], new CityGridPosition(0, 0));
+                // Ensure 3 points
+                Assert.IsTrue(lastBuilding.CalcScore(a) == 3);
 
-            // Build on col C
-            lastBuilding = game.BuildAt(game.buildingInfo[BuildingTypes.Beach], new CityGridPosition(2, 0));
-            // Ensure 1 points
-            Assert.IsTrue(lastBuilding.CalcScore(a) == 1);
+                // Build on col B (middle)
+                lastBuilding = game.BuildAt(game.buildingInfo[BuildingTypes.Beach], new CityGridPosition(1, 0));
+                // Ensure 1 points
+                Assert.IsTrue(lastBuilding.CalcScore(a) == 1);
 
-            // Build on col D
-            lastBuilding = game.BuildAt(game.buildingInfo[BuildingTypes.Beach], new CityGridPosition(3, 0));
-            // Ensure 3 points
-            Assert.IsTrue(lastBuilding.CalcScore(a) == 3);
+                // Build on last col
+                lastBuilding = game.BuildAt(game.buildingInfo[BuildingTypes.Beach],
+                        new CityGridPosition(width - 1, 0));
+                // Ensure 3 points
+                Assert.IsTrue(lastBuilding.CalcScore(a) == 3);
+            }
         }
 
         /// <summary>
