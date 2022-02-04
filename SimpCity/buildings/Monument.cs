49d43a05af138ebca3eb723fa48a4244dbbab6a6
s10193293@connect.np.edu.sg <s10193293@connect.np.edu.sg>
2022-02-04 19:58:04 +0800

    implemented second part but still under testing. First part of monument scoring can be tested.

--- SimpCity/buildings/Monument.cs
+++ SimpCity/buildings/Monument.cs
@@ -1,4 +1,5 @@
-﻿namespace SimpCity.buildings {
+﻿using System;
+namespace SimpCity.buildings {
     public class Monument : CityGridBuilding {
         public static string Name { get; } = "Monument";
         /// <summary>
@@ -6,22 +7,33 @@
         /// </summary>
         public static string Code { get; } = "MON";
         public Monument(BuildingInfo info) : base(info) { }
+        
         public override int CalcScore(ScoreCalculationArchive archive) {
             //initialise score to 0
             int score = 0;
             int xPos = Position().X;
             int yPos = Position().Y;
-            //corners
-            if(xPos == 0 && yPos == 0 || xPos == 0 && yPos == (Info.Grid.Height - 1) || xPos == (Info.Grid.Width - 1) && yPos == (Info.Grid.Height - 1) ||
-                xPos == (Info.Grid.Width - 1) && yPos == 0)
+            Console.WriteLine(xPos);
+            Console.WriteLine(yPos);
+            if ((xPos == 0 && yPos == 0) && (xPos == 0 && yPos == (Info.Grid.Height - 1)) && (xPos == (Info.Grid.Width - 1) && yPos == (Info.Grid.Height - 1)) &&
+                    (xPos == (Info.Grid.Width - 1) && yPos == 0)) {
+                return 4;
+            }
+            else 
             {
-                score += 2;
+                //corners
+                if ((xPos == 0 && yPos == 0) || (xPos == 0 && yPos == (Info.Grid.Height - 1)) || (xPos == (Info.Grid.Width - 1) && yPos == (Info.Grid.Height - 1)) ||
+                    (xPos == (Info.Grid.Width - 1) && yPos == 0))
+                    {
+                    score += 2;
+                }
+                //anywhere on grid except corners
+                else {
+                    score += 1;
+                }
+                return score;
             }
-            //anywhere on grid except corners
-            else {
-                score += 1;
-            }
-            return score;
+            
         }
     }
 }
